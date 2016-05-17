9ba31668db16a3afcdece23365bef6028f18e0e9
yogendratamang48 <yogen48@hotmail.com>
2016-05-17 11:43:09 +0545

    Check Box Checked After Adding new Row

---
+++ CheckBoxTest.aspx
@@ -0,0 +1,93 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckBoxTest.aspx.cs" Inherits="RoleTest.CheckBoxTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="margin: 0 auto; text-align: center">
+        <asp:ScriptManager ID="smMain" runat="server">
+        </asp:ScriptManager>
+        <asp:UpdatePanel ID="upgrvWorkatHouse" runat="server">
+            <ContentTemplate>
+                <table style="width: 1171px; border: 1px solid gray; margin: 5px auto; padding-top: 0;
+                    padding-bottom: 10px;" cellpadding="4" cellspacing="0">
+                    <tr>
+                        <td style="width: 1171px; text-align: left; font-size: small" valign="middle">
+                            तपाईको परिवारमा निम्न कार्यहरु प्राय कसले गर्दछ ? (दुवै भए दुवैमा ठिक लगाउने)
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="width: 1171px; text-align: center">
+                            <asp:GridView ID="grvWorkatHouse" runat="server" ShowFooter="True" AutoGenerateColumns="False"
+                                CellPadding="2" ForeColor="#333333" Width="100%" GridLines="None" Font-Size="14px"
+                                OnRowDeleting="grvWorkatHouse_RowDeleting" Style="text-align: left">
+                                <Columns>
+                                    <asp:BoundField DataField="RowNumber" HeaderText="क्र.सं" HeaderStyle-HorizontalAlign="Center"
+                                        HeaderStyle-VerticalAlign="Middle">
+                                        <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" Height="30" />
+                                        <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" Font-Names="Fontasy Himali" />
+                                    </asp:BoundField>
+                                    <asp:TemplateField HeaderText="कार्य" HeaderStyle-HorizontalAlign="Center" HeaderStyle-VerticalAlign="Middle"
+                                        ItemStyle-HorizontalAlign="Center" ItemStyle-VerticalAlign="Middle">
+                                        <ItemTemplate>
+                                            <asp:DropDownList ID="ddlgrvWorkatHouseWork" runat="server" CssClass="textBox" Height="25px"
+                                                Width="300px">
+                                                <asp:ListItem>------</asp:ListItem>
+                                                <asp:ListItem>घर व्यवहार सम्बन्धि विषयमा निर्णय</asp:ListItem>
+                                                <asp:ListItem>घरायसी काममा संलग्न</asp:ListItem>
+                                                <asp:ListItem>बैंकमा खाता संचालन</asp:ListItem>
+                                                <asp:ListItem>उपभोक्ता समितिमा सहभागिता</asp:ListItem>
+                                                <asp:ListItem>विद्धालय व्यवस्थापनमा सहभागीता</asp:ListItem>
+                                                <asp:ListItem>उद्धोग व्यापारमा सहभागीता</asp:ListItem>
+                                            </asp:DropDownList>
+                                        </ItemTemplate>
+                                        <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                        <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                    </asp:TemplateField>
+                                    <asp:TemplateField HeaderText="पुरुष" HeaderStyle-HorizontalAlign="Center" HeaderStyle-VerticalAlign="Middle"
+                                        ItemStyle-HorizontalAlign="Center" ItemStyle-VerticalAlign="Middle">
+                                        <ItemTemplate>
+                                            <asp:CheckBox ID="ckbgrvWorkatHouseMale" runat="server" Text="पुरुष" />
+                                        </ItemTemplate>
+                                        <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                        <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                    </asp:TemplateField>
+                                    <asp:TemplateField HeaderText="महिला" HeaderStyle-HorizontalAlign="Center" HeaderStyle-VerticalAlign="Middle"
+                                        ItemStyle-HorizontalAlign="Center" ItemStyle-VerticalAlign="Middle">
+                                        <ItemTemplate>
+                                            <asp:CheckBox ID="ckbgrvWorkatHouseFemale" runat="server" Text="महिला" />
+                                        </ItemTemplate>
+                                        <FooterStyle HorizontalAlign="Right" />
+                                        <FooterTemplate>
+                                            <asp:Button ID="grvWorkatHouseAdd" runat="server" Font-Names="Mangal" Text="थप्नुहोस्‌" Width="85px" Height="30px" BorderStyle="None" OnClick="grvWorkatHouseAdd_Click" />
+                                        </FooterTemplate>
+                                        <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                        <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                                    </asp:TemplateField>
+                                    <asp:CommandField ShowDeleteButton="True" ButtonType="Image" DeleteImageUrl="~/Icons/Delete.gif" />
+                                </Columns>
+                                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                                <RowStyle BackColor="#E3EAEB" />
+                                <EditRowStyle BackColor="#7C6F57" />
+                                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                                <AlternatingRowStyle BackColor="White" />
+                                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                                <SortedDescendingHeaderStyle BackColor="#15524A" />
+                            </asp:GridView>
+                        </td>
+                    </tr>
+                </table>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </div>
+    </form>
+</body>
+</html>
---
+++ CheckBoxTest.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RoleTest
+{
+    public partial class CheckBoxTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                grvWorkatHouseFirstGridViewRow();
+            }
+        }
+        #region For ३८. तपाईको परिवारमा निम्न कार्यहरु प्राय कसले गर्दछ ? (दुवै भए दुवैमा ठिक लगाउने)(grvWorkatHouse)
+        private void grvWorkatHouseFirstGridViewRow()
+        {
+            DataTable dt = new DataTable();
+            DataRow dr = null;
+            dt.Columns.Add(new DataColumn("RowNumber", typeof(string)));
+            dt.Columns.Add(new DataColumn("Col1", typeof(string)));
+            dt.Columns.Add(new DataColumn("Col2", typeof(string)));
+            dt.Columns.Add(new DataColumn("Col3", typeof(string)));
+            dr = dt.NewRow();
+            dr["RowNumber"] = 1;
+            dr["Col1"] = string.Empty;
+            dr["Col2"] = string.Empty;
+            dr["Col3"] = string.Empty;
+            dt.Rows.Add(dr);
+            ViewState["CurrentTablegrvWorkatHouse"] = dt;
+            grvWorkatHouse.DataSource = dt;
+            grvWorkatHouse.DataBind();
+        }
+        private void grvWorkatHouseAddNewRow()
+        {
+            int rowIndex = 0;
+            if (ViewState["CurrentTablegrvWorkatHouse"] != null)
+            {
+                DataTable dtCurrentTable = (DataTable)ViewState["CurrentTablegrvWorkatHouse"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        DropDownList ddlgrvWorkatHouseWork = (DropDownList)grvWorkatHouse.Rows[rowIndex].Cells[1].FindControl("ddlgrvWorkatHouseWork");
+                        CheckBox ckbgrvWorkatHouseMale = (CheckBox)grvWorkatHouse.Rows[rowIndex].Cells[2].FindControl("ckbgrvWorkatHouseMale");
+                        CheckBox ckbgrvWorkatHouseFemale = (CheckBox)grvWorkatHouse.Rows[rowIndex].Cells[3].FindControl("ckbgrvWorkatHouseFemale");
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["RowNumber"] = i + 1;
+                        dtCurrentTable.Rows[i - 1]["Col1"] = ddlgrvWorkatHouseWork.SelectedValue;
+                        dtCurrentTable.Rows[i - 1]["Col2"] = ckbgrvWorkatHouseMale.Checked;
+                        dtCurrentTable.Rows[i - 1]["Col3"] = ckbgrvWorkatHouseFemale.Checked;
+                        rowIndex++;
+                    }
+                    dtCurrentTable.Rows.Add(drCurrentRow);
+                    ViewState["CurrentTablegrvWorkatHouse"] = dtCurrentTable;
+                    grvWorkatHouse.DataSource = dtCurrentTable;
+                    grvWorkatHouse.DataBind();
+                }
+            }
+            else
+            {
+                Response.Write("ViewState is null");
+            }
+            grvWorkatHouseSetPreviousData();
+        }
+        private void grvWorkatHouseSetPreviousData()
+        {
+            int rowIndex = 0;
+            if (ViewState["CurrentTablegrvWorkatHouse"] != null)
+            {
+                DataTable dt = (DataTable)ViewState["CurrentTablegrvWorkatHouse"];
+                if (dt.Rows.Count > 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        DropDownList ddlgrvWorkatHouseWork = (DropDownList)grvWorkatHouse.Rows[rowIndex].Cells[1].FindControl("ddlgrvWorkatHouseWork");
+                        CheckBox ckbgrvWorkatHouseMale = (CheckBox)grvWorkatHouse.Rows[rowIndex].Cells[2].FindControl("ckbgrvWorkatHouseMale");
+                        
+                        CheckBox ckbgrvWorkatHouseFemale = (CheckBox)grvWorkatHouse.Rows[rowIndex].Cells[3].FindControl("ckbgrvWorkatHouseFemale");
+                        if (i<dt.Rows.Count - 1)
+                        {
+
+
+                            ckbgrvWorkatHouseMale.Checked = Convert.ToBoolean(dt.Rows[i][2].ToString());
+                            ckbgrvWorkatHouseFemale.Checked = Convert.ToBoolean(dt.Rows[i][3].ToString());
+                        }
+
+                        grvWorkatHouse.Rows[i].Cells[0].Text = Convert.ToString(i + 1);
+                        ddlgrvWorkatHouseWork.SelectedValue = dt.Rows[i]["Col1"].ToString();
+
+                        rowIndex++;
+                    }
+                }
+            }
+        }
+        protected void grvWorkatHouseAdd_Click(object sender, EventArgs e)
+        {
+            grvWorkatHouseAddNewRow();
+        }
+        protected void grvWorkatHouse_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            grvWorkatHouseSetRowData();
+            if (ViewState["CurrentTablegrvWorkatHouse"] != null)
+            {
+                DataTable dt = (DataTable)ViewState["CurrentTablegrvWorkatHouse"];
+                DataRow drCurrentRow = null;
+                int rowIndex = Convert.ToInt32(e.RowIndex);
+                if (dt.Rows.Count > 1)
+                {
+                    dt.Rows.Remove(dt.Rows[rowIndex]);
+                    drCurrentRow = dt.NewRow();
+                    ViewState["CurrentTablegrvWorkatHouse"] = dt;
+                    grvWorkatHouse.DataSource = dt;
+                    grvWorkatHouse.DataBind();
+
+                    for (int i = 0; i < grvWorkatHouse.Rows.Count - 1; i++)
+                    {
+                        grvWorkatHouse.Rows[i].Cells[0].Text = Convert.ToString(i + 1);
+                    }
+                    grvWorkatHouseSetPreviousData();
+                }
+            }
+        }
+        private void grvWorkatHouseSetRowData()
+        {
+            int rowIndex = 0;
+
+            if (ViewState["CurrentTablegrvWorkatHouse"] != null)
+            {
+                DataTable dtCurrentTable = (DataTable)ViewState["CurrentTablegrvWorkatHouse"];
+                DataRow drCurrentRow = null;
+                if (dtCurrentTable.Rows.Count > 0)
+                {
+                    for (int i = 1; i <= dtCurrentTable.Rows.Count; i++)
+                    {
+                        DropDownList ddlgrvWorkatHouseWork = (DropDownList)grvWorkatHouse.Rows[rowIndex].Cells[1].FindControl("ddlgrvWorkatHouseWork");
+                        CheckBox ckbgrvWorkatHouseMale = (CheckBox)grvWorkatHouse.Rows[rowIndex].Cells[2].FindControl("ckbgrvWorkatHouseMale");
+                        CheckBox ckbgrvWorkatHouseFemale = (CheckBox)grvWorkatHouse.Rows[rowIndex].Cells[3].FindControl("ckbgrvWorkatHouseFemale");
+                        drCurrentRow = dtCurrentTable.NewRow();
+                        drCurrentRow["RowNumber"] = i + 1;
+                        dtCurrentTable.Rows[i - 1]["Col1"] = ddlgrvWorkatHouseWork.SelectedValue;
+                        dtCurrentTable.Rows[i - 1]["Col2"] = ckbgrvWorkatHouseMale.Checked;
+                        dtCurrentTable.Rows[i - 1]["Col3"] = ckbgrvWorkatHouseFemale.Checked;
+                        rowIndex++;
+                    }
+                    ViewState["CurrentTablegrvWorkatHouse"] = dtCurrentTable;
+                }
+            }
+            else
+            {
+                Response.Write("ViewState is null");
+            }
+        }
+        #endregion
+    }
+}
